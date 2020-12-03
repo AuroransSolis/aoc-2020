07c219187f2e2725b00d2448ac42bd7e6c24e39f
Aurorans Solis <primalucegd@gmail.com>
2020-12-02 17:39:46 -0700

    Threw in Rayon for gits and shiggles.

--- days/src/day_2.rs
+++ days/src/day_2.rs
@@ -1,4 +1,5 @@
 use aoc_runner_derive::aoc;
+use rayon::{iter::ParallelIterator, str::ParallelString};
 
 fn get_parts(line: &str) -> ([usize; 2], char, &str) {
     let mut sections = line.split_whitespace();
@@ -10,22 +11,54 @@ fn get_parts(line: &str) -> ([usize; 2], char, &str) {
     ([min, max], test_char, password)
 }
 
-#[aoc(day2, part1)]
-pub fn part1(input: &str) -> usize {
+#[aoc(day2, part1, singlethreaded)]
+pub fn part1_st(input: &str) -> usize {
     input
         .lines()
         .filter(|line| {
             let ([min, max], test_char, password) = get_parts(line);
-            let count = password.as_bytes().iter().filter(|&&b| b == (test_char as u8)).count();
+            let count = password
+                .as_bytes()
+                .iter()
+                .filter(|&&b| b == (test_char as u8))
+                .count();
             count >= min && count <= max
         })
         .count()
 }
 
-#[aoc(day2, part2)]
-pub fn part2(input: &str) -> usize {
+#[aoc(day2, part1, parlines)]
+pub fn part1_pl(input: &str) -> usize {
+    input
+        .par_lines()
+        .filter(|line| {
+            let ([min, max], test_char, password) = get_parts(line);
+            let count = password
+                .as_bytes()
+                .iter()
+                .filter(|&&b| b == (test_char as u8))
+                .count();
+            count >= min && count <= max
+        })
+        .count()
+}
+
+#[aoc(day2, part2, singlethreaded)]
+pub fn part2_st(input: &str) -> usize {
     input
         .lines()
+        .filter(|line| {
+            let ([loc1, loc2], test_char, password) = get_parts(line);
+            (password.as_bytes()[loc1 - 1] == test_char as u8)
+                ^ (password.as_bytes()[loc2 - 1] == test_char as u8)
+        })
+        .count()
+}
+
+#[aoc(day2, part2, parlines)]
+pub fn part2_mt(input: &str) -> usize {
+    input
+        .par_lines()
         .filter(|line| {
             let ([loc1, loc2], test_char, password) = get_parts(line);
             (password.as_bytes()[loc1 - 1] == test_char as u8)
