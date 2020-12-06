ac69b0f2ab336bdb04313253ac712ad757a68bd4
Aurorans Solis <primalucegd@gmail.com>
2020-12-05 22:10:02 -0700

    Done with d6. Forgot about intersections, oops.

--- days/src/day_6.rs
+++ days/src/day_6.rs
@@ -1,1 +1,35 @@
+use aoc_runner_derive::aoc;
+use std::collections::HashSet;
 
+#[aoc(day6, part1)]
+pub fn part1(input: &str) -> usize {
+    input.split("\n\n")
+        .map(|group| {
+            group
+                .lines()
+                .flat_map(|line| line.chars())
+                .collect::<HashSet<char>>()
+                .len()
+        })
+        .sum::<usize>()
+}
+
+#[aoc(day6, part2)]
+pub fn part2(input: &str) -> usize {
+    input.split("\n\n")
+        .map(|group| {
+            let mut all = group
+                .lines()
+                .map(|line| line.chars().collect::<HashSet<char>>())
+                .collect::<Vec<_>>();
+            let mut first = all.pop().unwrap();
+            all
+                .into_iter()
+                .for_each(|set| {
+                    let common = first.intersection(&set).copied().collect::<HashSet<_>>();
+                    first = common;
+                });
+            first.len()
+        })
+        .sum::<usize>()
+}
