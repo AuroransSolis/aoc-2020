2dc6fd3269944c7d8128c57c11d689c778ec6ae4
Aurorans Solis <primalucegd@gmail.com>
2020-12-12 21:57:16 -0700

    Started perf rewrite.

--- days/src/day_11.rs
+++ days/src/day_11.rs
@@ -28,99 +28,95 @@ const EIGHT_DIRS: [(isize, isize); 8] = [
     (-1, 1),
 ];
 
-fn y_x_flip_pos<'a>(
-    sim: &'a [Vec<Position>],
-    flips: &'a mut [Vec<bool>],
-) -> impl Iterator<Item = (usize, usize, &'a mut bool, Position)> + 'a {
-    flips
-        .iter_mut()
-        .enumerate()
-        .flat_map(|(y, row)| {
-            row.iter_mut()
-                .enumerate()
-                .map(move |(x, flip)| (y, x, flip))
-        })
-        .map(move |(y, x, flip)| (y, x, flip, sim[y][x]))
-}
-
 fn coords_within_lims(y_lim: usize, x_lim: usize, y: isize, x: isize) -> bool {
     y >= 0 && y_lim as isize > y && x >= 0 && x_lim as isize > x
 }
 
 #[aoc_generator(day11)]
-fn input_generator(input: &str) -> (Vec<Vec<Position>>, Vec<Vec<bool>>) {
-    let seats = input
-        .lines()
-        .map(|line| {
-            line.chars()
-                .map(|c| match c {
-                    '.' => Position::Floor,
-                    'L' => Position::Empty,
-                    '#' => Position::Occupied,
-                    _ => panic!(format!("Unknown char: {}", c)),
-                })
-                .collect::<Vec<_>>()
+fn input_generator(input: &str) -> (usize, Vec<Position>, Vec<bool>) {
+    let mut lines = input.lines();
+    let mut seats = Vec::new();
+    let width = lines
+        .next()
+        .unwrap()
+        .chars()
+        .map(|c| match c {
+            '#' => Position::Occupied,
+            'L' => Position::Empty,
+            '.' => Position::Floor,
+            other => panic!(format!("Unknown character: {}", other)),
         })
-        .collect::<Vec<_>>();
-    let mut flips = Vec::with_capacity(seats.len());
-    (0..flips.capacity()).for_each(|i| flips.push(vec![false; seats[i].len()]));
-    (seats, flips)
+        .inspect(|&c| seats.push(c))
+        .count();
+    lines
+        .flat_map(|line| line.chars())
+        .map(|c| match c {
+            '#' => Position::Occupied,
+            'L' => Position::Empty,
+            '.' => Position::Floor,
+            other => panic!(format!("Unknown character: {}", other)),
+        })
+        .for_each(|c| seats.push(c));
+    let flips = vec![false; seats.len()];
+    (width, seats, flips)
 }
 
-fn sim_step_p1(sim: &[Vec<Position>], flips: &mut [Vec<bool>]) -> bool {
-    y_x_flip_pos(sim, flips)
+fn sim_step_p1(width: usize, sim: &[Position], flips: &mut [bool]) -> bool {
+    (0..sim.len() / width)
+        .flat_map(|y| (0..width).map(move |x| (y, x)))
+        .map(|(y, x)| (y, x, sim[y * width + x]))
         .filter(|&(.., spot)| spot != Position::Floor)
-        .map(|(y, x, flip, spot)| {
+        .map(|(y, x, spot)| {
             (
-                flip,
+                y * width + x,
                 spot,
                 EIGHT_DIRS
                     .iter()
                     .map(|(dy, dx)| (y as isize + dy, x as isize + dx))
-                    .filter(|&(y, x)| coords_within_lims(sim.len(), sim[0].len(), y, x))
+                    .filter(|&(y, x)| coords_within_lims(sim.len() / width, width, y, x))
                     .map(|(y, x)| (y as usize, x as usize))
-                    .map(|(y, x)| sim[y][x])
+                    .map(|(y, x)| sim[y * width + x])
                     .filter(|&position| position == Position::Occupied)
                     .count(),
             )
         })
-        .map(|(flip, spot, occ_adj)| {
+        .map(|(ind, spot, occ_adj)| {
             (
-                flip,
+                ind,
                 (spot == Position::Occupied && occ_adj > 3)
                     || (spot == Position::Empty && occ_adj == 0),
             )
         })
-        .map(|(flip, needs_flip)| (*flip = needs_flip, needs_flip))
+        .map(|(ind, needs_flip)| (flips[ind] = needs_flip, needs_flip))
         .fold(false, |needs_flips, (_, needs_flip)| {
             needs_flips || needs_flip
         })
 }
 
 #[aoc(day11, part1)]
-pub fn part1((seats, flips): &(Vec<Vec<Position>>, Vec<Vec<bool>>)) -> usize {
+pub fn part1((width, seats, flips): &(usize, Vec<Position>, Vec<bool>)) -> usize {
     let mut seats = seats.clone();
     let mut flips = flips.clone();
-    while sim_step_p1(&seats, &mut flips) {
+    while sim_step_p1(*width, &seats, &mut flips) {
         seats
             .iter_mut()
-            .flat_map(|row| row.iter_mut())
-            .zip(flips.iter().flat_map(|row| row.iter()))
+            .zip(flips.iter())
             .filter(|&(_, &flip)| flip)
             .for_each(|(spot, _)| spot.flip());
     }
     seats
         .into_iter()
-        .flat_map(|row| row.into_iter())
         .filter(|&pos| pos == Position::Occupied)
         .count()
 }
 
-fn sim_step_p2(sim: &[Vec<Position>], flips: &mut [Vec<bool>]) -> bool {
-    y_x_flip_pos(sim, flips)
-        .map(|(y, x, flip, spot)| {
+fn sim_step_p2(width: usize, sim: &[Position], flips: &mut [bool]) -> bool {
+    (0..sim.len() / width)
+        .flat_map(|y| (0..width).map(move |x| (y, x)))
+        .map(|(y, x)| (y, x, sim[y * width + x]))
+        .map(|(y, x, spot)| {
             (
-                flip,
+                y * width + x,
                 spot,
                 EIGHT_DIRS
                     .iter()
@@ -128,9 +124,9 @@ fn sim_step_p2(sim: &[Vec<Position>], flips: &mut [Vec<bool>]) -> bool {
                         (1..)
                             .map(move |mul| (dy * mul, dx * mul))
                             .map(|(dy, dx)| (y as isize + dy, x as isize + dx))
-                            .take_while(|&(y, x)| coords_within_lims(sim.len(), sim[0].len(), y, x))
+                            .take_while(|&(y, x)| coords_within_lims(sim.len() / width, width, y, x))
                             .map(|(y, x)| (y as usize, x as usize))
-                            .map(|(y, x)| sim[y][x])
+                            .map(|(y, x)| sim[y * width + x])
                             .find(|&position| position != Position::Floor)
                             .map(|position| position == Position::Occupied)
                             .unwrap_or(false)
@@ -139,34 +135,32 @@ fn sim_step_p2(sim: &[Vec<Position>], flips: &mut [Vec<bool>]) -> bool {
                     .count(),
             )
         })
-        .map(|(flip, spot, occ_adj)| {
+        .map(|(ind, spot, occ_adj)| {
             (
-                flip,
+                ind,
                 (spot == Position::Occupied && occ_adj > 4)
                     || (spot == Position::Empty && occ_adj == 0),
             )
         })
-        .map(|(flip, needs_flip)| (*flip = needs_flip, needs_flip))
+        .map(|(ind, needs_flip)| (flips[ind] = needs_flip, needs_flip))
         .fold(false, |needs_flips, (_, needs_flip)| {
             needs_flips || needs_flip
         })
 }
 
 #[aoc(day11, part2)]
-pub fn part2((seats, flips): &(Vec<Vec<Position>>, Vec<Vec<bool>>)) -> usize {
+pub fn part2((width, seats, flips): &(usize, Vec<Position>, Vec<bool>)) -> usize {
     let mut seats = seats.clone();
     let mut flips = flips.clone();
-    while sim_step_p2(&seats, &mut flips) {
+    while sim_step_p2(*width, &seats, &mut flips) {
         seats
             .iter_mut()
-            .flat_map(|row| row.iter_mut())
-            .zip(flips.iter().flat_map(|row| row.iter()))
+            .zip(flips.iter())
             .filter(|&(_, &flip)| flip)
             .for_each(|(spot, _)| spot.flip());
     }
     seats
         .into_iter()
-        .flat_map(|row| row.into_iter())
         .filter(|&pos| pos == Position::Occupied)
         .count()
 }
