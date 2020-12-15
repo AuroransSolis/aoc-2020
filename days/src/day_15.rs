4cab4295b99c06c0a0e9fd4416f9b1589b58813b
Aurorans Solis <primalucegd@gmail.com>
2020-12-15 00:02:09 -0700

    p2 sucked :(

--- days/src/day_15.rs
+++ days/src/day_15.rs
@@ -1,1 +1,46 @@
+use aoc_runner_derive::aoc;
+use rayon::prelude::*;
 
+#[aoc(day15, part1)]
+pub fn part1(input: &str) -> usize {
+    let mut nums = input
+        .split(',')
+        .map(|num| num.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+    let mut ind = nums.len() - 1;
+    while nums.len() < 2020 {
+        if let Some(pos) = nums[0..ind].iter().rposition(|&n| n == nums[ind]) {
+            nums.push(ind - pos);
+        } else {
+            nums.push(0);
+        }
+        ind += 1;
+    }
+    nums[nums.len() - 1]
+}
+
+use std::collections::HashMap;
+
+#[aoc(day15, part2)]
+pub fn part2(input: &str) -> usize {
+    let mut nums = HashMap::with_capacity(30000000);
+    let mut last_num = 0;
+    input
+        .split(',')
+        .map(|num| num.parse::<usize>().unwrap())
+        .inspect(|&num| last_num = num)
+        .enumerate()
+        .for_each(|(pos, num)| drop(nums.insert(num, (0, pos))));
+    for count in nums.len()..30_000_000 {
+        let &(since_last, _) = nums.get(&last_num).unwrap();
+        nums
+            .entry(since_last)
+            .and_modify(|(since_last, said_when)| {
+                *since_last = count - *said_when;
+                *said_when = count;
+            })
+            .or_insert((0, count));
+        last_num = since_last;
+    }
+    last_num
+}
